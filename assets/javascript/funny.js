b857c25b0c3cea6fb0f16f670b2c81a081c79491
xkt <118078616+Xukt@users.noreply.github.com>
2024-04-13 13:19:18 +0100

    Update funny.js

--- assets/javascript/funny.js
+++ assets/javascript/funny.js
@@ -3,27 +3,27 @@ const body = document.getElementsByTagName("body");
 window.addEventListener("keydown", event => {
   if (event.ctrlKey && (event.key === "S" || event.key === "s")) {
     event.preventDefault();
-    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat";
+    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat. xkt on discord";
   }
   if (event.ctrlKey && event.key === "C") {
     event.preventDefault();
-    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat";
+    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat. xkt on discord";
   }
   if (event.ctrlKey && (event.key === "E" || event.key === "e")) {
     event.preventDefault();
-    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat";
+    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat. xkt on discord";
   }
   if (event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
     event.preventDefault();
-    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat";
+    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat. xkt on discord";
   }
   if (event.ctrlKey && (event.key === "K" || event.key === "k")) {
     event.preventDefault();
-    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat";
+    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat. xkt on discord";
   }
   if (event.ctrlKey && (event.key === "U" || event.key === "u")) {
     event.preventDefault();
-    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat";
+    body[0].innerHTML = "quit trying to get the source you fat fuck, xkt_074 on snapchat. xkt on discord";
   }
 });
 document.addEventListener("contextmenu", function (event) {
